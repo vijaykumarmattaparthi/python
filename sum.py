23f5a13f3d06c0c14297f6bde04b97f995c2355a
vijaykumarmattaparthi <vijaykumarmattaparthy@gmail.com>
2025-10-20 23:22:50 +0530

    these are my python scripts

---
+++ sum.py
@@ -0,0 +1,5 @@
+Python 3.12.9 (main, Aug 14 2025, 00:00:00) [GCC 14.2.1 20250110 (Red Hat 14.2.1-7)] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=4
+>>> b=5
+>>> sum = a + b
---
+++ sum2.py
@@ -0,0 +1,6 @@
+a=100
+b=200
+sum = a + b
+print (sum)
+
+
