fb9e2092bdcbfcbfc54b341d77be7a10fd2f49f4
vijay <vijaykumarmattaparthy@gmail.com>
2025-11-25 22:12:08 +0530

    allops

---
+++ allops.py
@@ -0,0 +1,10 @@
+a = int(input("Enter value of A:"))
+b = int(input("Enter value of B:"))
+sum = a+b
+sub = a-b
+mul = a*b
+div = a/b
+fld = a//b
+pdv = a%b
+pwr = a**b
+print(sum,sub,mul,div,fld,pdv,pwr)
